6818d7cbb660cd85351668e2de71a236310b9699
Victor Gunnarsson <victor@educar.se>
2022-09-06 17:55:03 +0200

    test

--- src/pages/victor.tsx
+++ src/pages/victor.tsx
@@ -3,7 +3,7 @@ interface Properties {}
 function Victor(properties: Properties) {
 	return (
 		<div>
-			<h1 className='text-3xl font-bold underline'>This is a test!!</h1>
+			<h1 className='text-3xl font-bold underline'>This is a test!</h1>
 		</div>
 	)
 }
